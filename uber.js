a78495b0f3079e8af9dde77f4545d8a145191844
Akshay Rawat <155710637+Akshay-Rawat@users.noreply.github.com>
2026-03-14 15:46:52 +0530

    i

--- uber.js
+++ uber.js
@@ -1,19 +1,34 @@
-class Uber{
-    static total=0
-    static #driver
-    constructor(name,type,rating,avail){
-        this.name=name
-        this.type=type
-        this.rating=rating
-        this.avail=avail
-        Uber.total++
-    }
-
-    ride(pickup,drop,fare,status){
-        fare=(pickup-drop)*9
-    }
-
-    static setDriver(#driver){
-        
+class Driver {
+    constructor(name, type, rating, avail) {
+        this.name = name
+        this.type = type
+        this.rating = rating
+        this.avail = avail
     }
 }
+    const pricePerKm = {
+        mini: 8,
+        auto: 10,
+        premium: 15
+
+}
+class Rider {
+    constructor(name, history=[]) {
+        this.name = name
+        this.history = history
+    }
+
+}
+class Ride {
+    constructor(pickup, drop, type) {
+        this.pickup = pickup
+        this.drop = drop
+        this.type = type
+        this.distance = Math.abs(drop - pickup)
+        this.fare = this.distance * pricePerKm[type]
+        this.status = "requested"
+    }
+}
+
+let ride = new Ride(1,13,"mini")
+console.log(ride)
